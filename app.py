e52ac0fc464be3bf09307a2da113ea788feb7bbd
Paweł Drozgowski <drozgowski@digital-alpaca.com>
2021-01-29 21:35:54 +0100

    Dodano wybor jezyka w menu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QPlainTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QPlainTextEdit, QComboBox
 from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QFont
 import sys
@@ -47,21 +47,16 @@ class Application():
 
         # Code
         self.labelCode = QLabel()
-
-        self.text= getRandomExample("python", "medium")
-        self.labelCode.setText(self.text)
-
-        self.labelCode.setFixedSize(640, 200)
+        self.labelCode.setFixedSize(640, 250)
         self.labelCode.setStyleSheet("background-color: #0a102e; color: #ffffff; padding: 10px; border-radius: 5px")
         self.labelCode.setFont(QFont('Arial', 18))
         self.labelCode.setAlignment(Qt.AlignLeft)
 
         # Input
         self.userInput = QPlainTextEdit()
-        self.userInput.setPlainText("")
         self.userInput.textChanged.connect(lambda: checkSpelling(self.text, self.userInput))
         
-        self.userInput.setMaximumHeight(300)
+        self.userInput.setFixedSize(640, 250)
         self.userInput.setStyleSheet("background-color: #596ed9; border: 1px solid #596ed9; border-radius: 5px;")
         self.userInput.setFont(QFont('Arial', 18))
         #self.userInput.textChanged.connect(lambda: checkSpelling(self.text, self.userInput))
@@ -81,7 +76,7 @@ class Application():
         :return: brak
         """
         self.menu = QWidget()
-        self.menu.setStyleSheet("QPushButton { background-color: #dbce18; color: #0e2a45; border: none; border-radius: 5px; width: 200px; height: 50px; }")
+        self.menu.setStyleSheet("QPushButton { background-color: #dbce18; color: #0e2a45; border: none; border-radius: 5px; width: 200px; height: 50px; } QComboBox { background-color: #596ed9; text-decoration: none; color: black; border: none; border-radius: 5px; } QAbstractItemView { background-color: #596ed9; }")
 
         self.menuLayout = QVBoxLayout()
 
@@ -90,24 +85,29 @@ class Application():
         self.menuLabelLogo.setFixedSize(600, 100)
         self.menuLabelLogo.setFont(QFont('Arial', 20))
         self.menuLabelLogo.setAlignment(Qt.AlignCenter)
-
         self.menuLayout.addWidget(self.menuLabelLogo)
+
+        self.select = QComboBox()
+        self.select.addItems(["Python", "C++", "Java"])
+        self.select.setFixedSize(200, 50)
+        self.select.setFont(QFont('Arial', 18))
+        self.menuLayout.addWidget(self.select)
 
         self.menuButton1 = QPushButton("Easy")
         self.menuButton1.setStyleSheet("background-color: #32a852")
         self.menuButton1.setFont(QFont('Arial', 18))
-        self.menuButton1.clicked.connect(self.startGame)
+        self.menuButton1.clicked.connect(lambda: self.startGame(self.menuButton1.text()))
         self.menuLayout.addWidget(self.menuButton1)
 
         self.menuButton2 = QPushButton("Medium")
         self.menuButton2.setFont(QFont('Arial', 18))
-        self.menuButton2.clicked.connect(self.startGame)
+        self.menuButton2.clicked.connect(lambda: self.startGame(self.menuButton2.text()))
         self.menuLayout.addWidget(self.menuButton2)
 
         self.menuButton3 = QPushButton("Hard")
         self.menuButton3.setStyleSheet("background-color: #ad3238")
         self.menuButton3.setFont(QFont('Arial', 18))
-        self.menuButton3.clicked.connect(self.startGame)
+        self.menuButton3.clicked.connect(lambda: self.startGame(self.menuButton3.text()))
         self.menuLayout.addWidget(self.menuButton3)
 
         self.menuButton4 = QPushButton("Exit")
@@ -117,6 +117,7 @@ class Application():
         self.menuLayout.addStretch()
         self.menuLayout.addWidget(self.menuButton4)
 
+        self.menuLayout.setAlignment(self.select, Qt.AlignHCenter)
         self.menuLayout.setAlignment(self.menuButton1, Qt.AlignHCenter)
         self.menuLayout.setAlignment(self.menuButton2, Qt.AlignHCenter)
         self.menuLayout.setAlignment(self.menuButton3, Qt.AlignHCenter)
@@ -129,7 +130,13 @@ class Application():
     def onTextChange(self):
         self.labelCode.setText(self.userInput.text())
 
-    def startGame(self):
+    def startGame(self, text):
+
+        language = self.select.currentText()
+        language = language.lower()
+        difficulty = text.lower()
+        self.text = getRandomExample(language, difficulty)
+        self.labelCode.setText(self.text)
         self.window.setCurrentWidget(self.game)
 
 app = Application()
